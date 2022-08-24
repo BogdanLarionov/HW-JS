3172337218efcfc5d00247798f338401db447d83
Bogdan Larionov <larionovbogdan1990@gmail.com>
2022-08-24 06:29:35 +0200

    first commit

---
+++ 1/learnJS.js
@@ -0,0 +1,71 @@
+// Домашнее задание:
+
+// 1 Сравнить две константы двумя разныеми способами:
+
+// Создать две константы, присвоив одной значение строкового типа, а второй числового
+// Сравнить эти констатны и вывести в консоль результат типа boolean так, чтобы
+// в одном случае( == ) он был true, а в другом( === ) false
+
+
+// const string = '1';
+// const number = 1;
+
+// console.log("number == string", number == string);
+// console.log("string === number", string === number);
+
+
+
+// const pistring = "3.14";
+// const piNumber = Number (pistring);
+// console.log(pistring == piNumber);
+// console.log(pistring === piNumber);
+
+
+
+// 2 Реализовать решение задачи в JS:
+
+// У нас есть 100 (создать константу)
+
+// Цена помидоров на рынке - 10 (создать константу)
+
+// Цена огурцов на рынке - 5 (создать константу)
+
+// Цена яблок на рынке - 15 (создать константу)
+
+// Оставшиеся деньги потратим на орехи (создать переменную)
+
+// Мы купили на рынке по два килограмма огурцов, помидоров и яблок (создать переменную)
+
+// оставшиеся деньги потратили на орехи (присвоить значение переменной)
+
+// сколько денег мы потратили на орехи? (вывести переменную в консоль)
+
+// на что мы потратили больше денег на орехи или остальное? (вывести boolean значение в консоль)
+
+// в зависимости от того потратили ли мы на орехи больше времени чем на всё остальное
+// выводите в консоль сообщение об этом 'Больше потратили на орехи' или 'Больше потратили на фрукты и овощи'
+
+// const money = 100;
+// const tomatosPrice = 10;
+// const cucumberPrice = 5;
+// const applePrice = 15;
+
+// const spentOnCucumbers = cucumberPrice * 2;
+// const spentOnTomatos = tomatosPrice * 2;
+// const spentOnApple = applePrice * 2;
+// const spentOnNuts = money - (spentOnCucumbers + spentOnTomatos + spentOnApple);
+
+// console.log(`Потрачено на орехи ${spentOnNuts}, ${spentOnNuts ? "Больше потратили на орехи" :
+//           "Больше потратили на фрукты и овощи"}`);
+
+
+
+
+const totalManey = 100;
+const tomatosPrice = 10;
+const cucumbersPrice = 5;
+const applePrice = 15;
+
+let m = null;
+
+
